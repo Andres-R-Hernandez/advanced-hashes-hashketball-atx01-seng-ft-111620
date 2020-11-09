5461661f9474cdc14c25c0f75bfabaeff96e73f7
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-09 19:22:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -193,8 +193,9 @@ def big_shoe_rebounds
   biggest_shoe = 0
   game_hash.each do |host, team_attributes|
     team_attributes[:players].each do |player_stats|
-      if player_stats[:player_name] == player_name
-        return player_stats
+      if player_stats[:shoe] > biggest_shoe
+        biggest_shoe = player_stats[:shoe]
+        rebounds_from_big_shoe = player_stats[:rebounds]
       end
     end
   end
