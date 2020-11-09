f469d85f42d8d2b6d63a0bf03117bc61c176e697
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-09 18:22:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,7 +128,14 @@ end
 
 # Write code here
 
-def num_points_scored
+def num_points_scored(player_name)
+  game_hash.each do |host|
+    host[:players].each do |player_stats|
+      if player_stats[:player_name] == player_name
+        host[:players][:player_name]
+      end
+    end
+  end
 end
 
 def shoe_size
